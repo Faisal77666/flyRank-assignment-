a91c803d1cf4ac153358ec138c4fbad345dfc701
Faisal77666 <sfm77666@gmail.com>
2026-09-11 11:06:47 +0500

    Stage 5: Swagger UI

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const openapiSpec = require("./openapi.json");
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
 // In-memory "database"
 let tasks = [
